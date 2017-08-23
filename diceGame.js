8f8efe08852dcff88aaa7c1e283fe747f1d38fd6
jaredburks <jarabu93@gmail.com>
2017-08-23 09:29:35 -0500

    Fixed index link to diceGame.js and completed 4-sided dice function

--- diceGame.js
+++ diceGame.js
@@ -9,11 +9,29 @@
 function fourSide(){
 	//dice needs to roll and have value saved
 	//need to have user guess the number and keep track of guesses
-	//for(var i = 1; i <= 3; i++ )
+	//for(var i = 1; i <= 2; i++ )
 		//if(playerGuess === dice value) you win
 		//else try again
 	//you lose guy
-	var dice = ;
-	var rollValue = ;
-	var playerGuess = prompt("Guess a number between 1 & 6:");
+	var dice = Math.floor((Math.random() * 4) + 1);
+	var note = console.log("You've selected the 4 sided dice.") ;
+
+	//console.log(dice);//Gives answer
+
+	for(var i = 0; i <= 2; i++){
+		var playerGuess = parseInt(prompt("Guess a number between 1 & 4:"));
+		if(playerGuess === dice){
+			console.log("Lucky Guess! You Win!");
+			return alert("Lucky Guess! You Win!");
+		}
+		else if(i < 2){
+			console.log("Nope! Try Again");
+			alert("Nope! Try Again");
+		}
+		else{
+			console.log("You lose Jabroni");
+			alert("You lose Jabroni");
+		}
+	}
 }
+fourSide();
