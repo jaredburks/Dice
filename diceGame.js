79ddd2ddae76ed832add005b1156d68792b157c8
jaredburks <jarabu93@gmail.com>
2017-08-23 07:51:39 -0500

    Added comments laying out the game plan, started 4-sided dice function with variables, and linked index with diceGame.js

---
+++ diceGame.js
@@ -0,0 +1,19 @@
+//Dice game where you guess the number on the dice
+
+//Game starts:Pick one of the 5 dice to play with
+//Dice rolls: you get a certain amount of guesses based on the dice's sides
+//If you guess right, you win.
+//If you guess wrong, try again.
+//Run out of guesses, you lose.
+
+function fourSide(){
+	//dice needs to roll and have value saved
+	//need to have user guess the number and keep track of guesses
+	//for(var i = 1; i <= 3; i++ )
+		//if(playerGuess === dice value) you win
+		//else try again
+	//you lose guy
+	var dice = ;
+	var rollValue = ;
+	var playerGuess = prompt("Guess a number between 1 & 6:");
+}
