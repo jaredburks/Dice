682721722f657e89130ae697f58a9c1531a218da
jaredburks <jarabu93@gmail.com>
2017-08-23 11:45:05 -0500

    Added the 5 other dice functions, which are variations of the first function

--- diceGame.js
+++ diceGame.js
@@ -40,6 +40,146 @@ function fourSide(){
 	}
 }
 
+function sixSide(){
+	var diceSides = 6;
+	var diceRoll = Math.floor((Math.random() * diceSides) + 1);
+	var guesses = diceSides / 2;
+	var note = console.log("You've selected the 6 sided dice.");
+
+	//console.log(dice);//Gives answer
+
+	for(var i = 1; i <= guesses; i++){
+		var playerGuess = parseInt(prompt("Guess a number between 1 & 6:"));
+		if(playerGuess === diceRoll){
+			console.log("Lucky Guess! You Win!");
+			displayWin();
+			return alert("Lucky Guess! You Win!");
+
+		}
+		else if(i < guesses){
+			console.log("Nope! Try Again");
+			alert("Nope! Try Again");
+		}
+		else{
+			console.log("You lose Jabroni");
+			alert("You lose Jabroni");
+			displayLost();
+		}
+	}
+}
+
+function eightSide(){
+	var diceSides = 8;
+	var diceRoll = Math.floor((Math.random() * diceSides) + 1);
+	var guesses = diceSides / 2;
+	var note = console.log("You've selected the 8 sided dice.");
+
+	//console.log(dice);//Gives answer
+
+	for(var i = 1; i <= guesses; i++){
+		var playerGuess = parseInt(prompt("Guess a number between 1 & 8:"));
+		if(playerGuess === diceRoll){
+			console.log("Lucky Guess! You Win!");
+			displayWin();
+			return alert("Lucky Guess! You Win!");
+
+		}
+		else if(i < guesses){
+			console.log("Nope! Try Again");
+			alert("Nope! Try Again");
+		}
+		else{
+			console.log("You lose Jabroni");
+			alert("You lose Jabroni");
+			displayLost();
+		}
+	}
+}
+
+function tenSide(){
+	var diceSides = 10;
+	var diceRoll = Math.floor((Math.random() * diceSides) + 1);
+	var guesses = diceSides / 2;
+	var note = console.log("You've selected the 10 sided dice.");
+
+	//console.log(dice);//Gives answer
+
+	for(var i = 1; i <= guesses; i++){
+		var playerGuess = parseInt(prompt("Guess a number between 1 & 10:"));
+		if(playerGuess === diceRoll){
+			console.log("Lucky Guess! You Win!");
+			displayWin();
+			return alert("Lucky Guess! You Win!");
+
+		}
+		else if(i < guesses){
+			console.log("Nope! Try Again");
+			alert("Nope! Try Again");
+		}
+		else{
+			console.log("You lose Jabroni");
+			alert("You lose Jabroni");
+			displayLost();
+		}
+	}
+}
+
+function twelveSide(){
+	var diceSides = 12;
+	var diceRoll = Math.floor((Math.random() * diceSides) + 1);
+	var guesses = diceSides / 2;
+	var note = console.log("You've selected the 12 sided dice.");
+
+	//console.log(dice);//Gives answer
+
+	for(var i = 1; i <= guesses; i++){
+		var playerGuess = parseInt(prompt("Guess a number between 1 & 12:"));
+		if(playerGuess === diceRoll){
+			console.log("Lucky Guess! You Win!");
+			displayWin();
+			return alert("Lucky Guess! You Win!");
+
+		}
+		else if(i < guesses){
+			console.log("Nope! Try Again");
+			alert("Nope! Try Again");
+		}
+		else{
+			console.log("You lose Jabroni");
+			alert("You lose Jabroni");
+			displayLost();
+		}
+	}
+}
+
+function twentySide(){
+	var diceSides = 20;
+	var diceRoll = Math.floor((Math.random() * diceSides) + 1);
+	var guesses = diceSides / 2;
+	var note = console.log("You've selected the 20 sided dice.");
+
+	//console.log(dice);//Gives answer
+
+	for(var i = 1; i <= guesses; i++){
+		var playerGuess = parseInt(prompt("Guess a number between 1 & 20:"));
+		if(playerGuess === diceRoll){
+			console.log("Lucky Guess! You Win!");
+			displayWin();
+			return alert("Lucky Guess! You Win!");
+
+		}
+		else if(i < guesses){
+			console.log("Nope! Try Again");
+			alert("Nope! Try Again");
+		}
+		else{
+			console.log("You lose Jabroni");
+			alert("You lose Jabroni");
+			displayLost();
+		}
+	}
+}
+
 function displayLost(){
 	var x = document.createElement("IMG");
 	x.setAttribute("src", "youlose.gif");
